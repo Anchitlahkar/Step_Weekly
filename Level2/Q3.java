c942de60e8091700a9e12dfea3dbdae0b9baa845
Anchit Lahkar <64302945+Anchitlahkar@users.noreply.github.com>
2026-03-11 09:56:02 +0530

    Refactor input handling to use try-with-resources for Scanner in Q1, Q3, Q5, and Q5 classes

--- Level2/Q3.java
+++ Level2/Q3.java
@@ -2,13 +2,15 @@
 import java.util.Scanner;
 
 public class Q3 {
+
     public static void main(String[] args) {
         double perimeter;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the perimeter of the square: ");
-        perimeter = input.nextDouble();
-        double side = perimeter / 4;
-        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
-        input.close();
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the perimeter of the square: ");
+            perimeter = input.nextDouble();
+            double side = perimeter / 4;
+            System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+            input.close();
+        }
     }
 }
